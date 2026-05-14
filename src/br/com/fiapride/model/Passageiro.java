bdf7b27103f181aca5c3599aebd64619cca7027c
Guimart1 <gui.martins.sts@gmail.com>
2026-05-14 20:25:55 -0300

    feat: aula3 - aplica encapsulamento com modificadores de acesso

--- src/br/com/fiapride/model/Passageiro.java
+++ src/br/com/fiapride/model/Passageiro.java
@@ -1,14 +1,16 @@
 package br.com.fiapride.model;
 
 public class Passageiro {
-    public String nome;
-    public String cpf; // Novo Atributo
-    public double saldo;
+
+    // Agora ninguém fora desta classe consegue mexer nisso diretamente!
+    private String nome;
+    private String cpf;
+    private double saldo;
 
     public Passageiro(String nome, String cpf) {
-        this.nome = nome;
-        this.cpf = cpf; // Novo Atributo
-        this.saldo = 0.00;
+        this.setNome(nome);
+        this.setCpf(cpf);
+        this.setSaldo(0); // Todo mundo começa com zero!
     }
 
     public void adicionarSaldo(double valor) {
@@ -33,5 +35,37 @@ public class Passageiro {
         }
         this.saldo -= custo;
         System.out.println("Viagem paga. Saldo restante: " + this.saldo);
+    }
+
+    // Método para PEGAR o saldo (Leitura)
+    public double getSaldo() {
+        return this.saldo; // Apenas devolve o valor, não altera nada.
+    }
+
+    // Método para DEFINIR o saldo (Escrita com Regra de Negócio!)
+    private void setSaldo(double valor) {
+        if (valor >= 0) {
+            this.saldo = valor;
+        } else {
+            System.out.println("Erro de Segurança: Tentativa de definir saldo negativo bloqueada!");
+        }
+    }
+
+    // Faça o mesmo para o nome (sem regras complexas por enquanto)
+    public String getNome() {
+        return this.nome;
+    }
+
+    private void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // Faça o mesmo para o cpf (sem regras complexas por enquanto)
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    private void setCpf(String cpf) {
+        this.cpf = cpf;
     }
 }
